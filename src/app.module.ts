995c73409d4ce7d77f9386b63c76af85af9e441c
Gabriel Leite <gabrielcfcleite@gmail.com>
2020-04-26 07:21:45 -0300

    Criação do módulo de fotos.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PhotoModule } from './photo/photo.module';
 
 @Module({
-  imports: [],
+  imports: [PhotoModule],
   controllers: [AppController],
   providers: [AppService],
 })
