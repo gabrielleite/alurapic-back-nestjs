5eec1b62a2b875b267ddbfc67cf1207181bd95dd
Gabriel Leite <gabrielcfcleite@gmail.com>
2020-04-26 07:25:24 -0300

    Criação do controller de fotos e do endpoint de listagem.

---
+++ src/photo/photo.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('photos')
+export class PhotoController {
+
+    private photos: Array<any> = [
+        {
+            id: 1,
+            url: 'https://p2.piqsels.com/preview/944/748/901/bird-parrot-cockatiel-yellow-thumbnail.jpg',
+            description: 'Calopsita do agreste.',
+            user: 'gabriel.leite@alura.com.br'
+        },
+        {
+            id: 2,
+            url: 'https://www.publicdomainpictures.net/pictures/30000/velka/lazy-lion.jpg',
+            description: 'Leão preguiçoso.',
+            user: 'gabriel.leite@alura.com.br'
+        },
+        {
+            id: 3,
+            url: 'https://cdn.pixabay.com/photo/2020/01/02/14/53/elephant-4736008_960_720.jpg',
+            description: 'Reunião de planejamento semanal.',
+            user: 'david.neves@alura.com.br'
+        }
+    ];
+
+    @Get()
+    public list(): Array<any> {
+        return this.photos;
+    }
+}
--- src/photo/photo.module.ts
+++ src/photo/photo.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { PhotoController } from './photo.controller';
 
-@Module({})
+@Module({
+  controllers: [PhotoController]
+})
 export class PhotoModule {}
